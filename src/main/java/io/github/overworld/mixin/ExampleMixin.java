22bd87d0245fb8d371e85938ffd6005a547e28e7
khalidnecron <bill.ciphermewtwo@gmail.com>
2023-02-22 01:30:53 -0500

    another commit

--- src/main/java/io/github/overworld/mixin/ExampleMixin.java
+++ src/main/java/io/github/overworld/mixin/ExampleMixin.java
@@ -1,16 +1,17 @@
-package net.fabricmc.example.mixin;
+package io.github.overworld.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import io.github.overworld.OverWorldMod;
+
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		OverWorldMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
