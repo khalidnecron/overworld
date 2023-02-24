aba6ea06d9c5d01f4e1727ea4c5ace6d1b0e0871
khalidnecron <bill.ciphermewtwo@gmail.com>
2023-02-24 06:52:27 -0500

    added cardinal componets and correction damage

--- src/main/java/io/github/overworld/items/InfernalReliquary.java
+++ src/main/java/io/github/overworld/items/InfernalReliquary.java
@@ -1,19 +1,23 @@
 package io.github.overworld.items;
 
 import java.util.UUID;
-
 import com.google.common.collect.Multimap;
-
-import dev.emi.trinkets.api.SlotAttributes;
+import dev.emi.trinkets.TrinketSlot;
 import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketInventory;
 import dev.emi.trinkets.api.TrinketItem;
+import dev.emi.trinkets.api.TrinketsApi;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.world.event.GameEvent;
 
 public class InfernalReliquary extends TrinketItem {
 
@@ -29,16 +33,43 @@ public class InfernalReliquary extends TrinketItem {
     }
 
     @Override
-    public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot,
+    public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference ref,
             LivingEntity entity, UUID uuid) {
         
-        Multimap<EntityAttribute, EntityAttributeModifier> modifiers = super.getModifiers(stack, slot, entity, uuid);
+        Multimap<EntityAttribute, EntityAttributeModifier> modifiers = super.getModifiers(stack, ref, entity, uuid);
         
         modifiers.put(EntityAttributes.GENERIC_MAX_HEALTH, new EntityAttributeModifier(uuid, "overworld:max_health", -6,  EntityAttributeModifier.Operation.ADDITION));
 
-        SlotAttributes.addSlotModifier(modifiers, "hand/ring", uuid, 1, EntityAttributeModifier.Operation.ADDITION);
-        SlotAttributes.addSlotModifier(modifiers, "offhand/ring", uuid, 1, EntityAttributeModifier.Operation.ADDITION);
-
         return modifiers;
     }
+
+   
+    public static boolean equipItem(PlayerEntity player, ItemStack stack) {
+        var optional = TrinketsApi.getTrinketComponent(player);
+		if (optional.isPresent()) {
+			TrinketComponent comp = optional.get();
+			for (var group : comp.getInventory().values()) {
+				for (TrinketInventory inv : group.values()) {
+					for (int i = 0; i < inv.size(); i++) {
+						if (inv.getStack(i).isEmpty()) {
+							SlotReference ref = new SlotReference(inv, i);
+							if (TrinketSlot.canInsert(stack, ref, player)) {
+								ItemStack newStack = stack.copy();
+								inv.setStack(i, newStack);
+								SoundEvent soundEvent = stack.getEquipSound();
+								if (!stack.isEmpty() && soundEvent != null) {
+								   player.emitGameEvent(GameEvent.EQUIP);
+								   player.playSound(soundEvent, 1.0F, 1.0F);
+                                   player.setHealth(player.getHealth() - 1);
+								}
+								stack.setCount(0);
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
