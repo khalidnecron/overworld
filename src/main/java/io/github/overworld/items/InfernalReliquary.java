13f9ff3de61d1f5c4322c43e16bee10261a15f6e
khalidnecron <bill.ciphermewtwo@gmail.com>
2023-02-23 22:30:21 -0500

    modifiers and update dependencies

--- src/main/java/io/github/overworld/items/InfernalReliquary.java
+++ src/main/java/io/github/overworld/items/InfernalReliquary.java
@@ -1,8 +1,15 @@
 package io.github.overworld.items;
 
+import java.util.UUID;
+
+import com.google.common.collect.Multimap;
+
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketItem;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemStack;
@@ -15,8 +22,19 @@ public class InfernalReliquary extends TrinketItem {
 
     @Override
     public void tick(ItemStack stack, SlotReference slot, LivingEntity entity) {
-        
+
         StatusEffectInstance effectInstance = new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 2, 0, false, false);
         entity.addStatusEffect(effectInstance);
+    }
+
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot,
+            LivingEntity entity, UUID uuid) {
+        
+        Multimap<EntityAttribute, EntityAttributeModifier> modifiers = super.getModifiers(stack, slot, entity, uuid);
+        
+        modifiers.put(EntityAttributes.GENERIC_MAX_HEALTH, new EntityAttributeModifier(uuid, "overworld:max_health", -6,  EntityAttributeModifier.Operation.ADDITION));
+
+        return modifiers;
     }
 }
