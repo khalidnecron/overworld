ebf1620dffb52dfef36b5fc5df28725cd071f856
khalidnecron <bill.ciphermewtwo@gmail.com>
2023-02-23 22:45:33 -0500

    register infernal reliquary

--- src/main/java/io/github/overworld/OverWorldMod.java
+++ src/main/java/io/github/overworld/OverWorldMod.java
@@ -1,21 +1,34 @@
 package io.github.overworld;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.github.overworld.items.InfernalReliquary;
+
 public class OverWorldMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("overworld");
+	public static final InfernalReliquary INFERNAL_RELIQUARY = new InfernalReliquary(
+		new FabricItemSettings().fireproof().rarity(Rarity.EPIC).maxCount(1)
+	);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Hello Fabric world!");
+
+		Registry.register(Registries.ITEM, new Identifier("overworld", "infernal_reliquary"), INFERNAL_RELIQUARY);
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> { 
+			content.add(INFERNAL_RELIQUARY);
+		});
 	}
 }
