f751f0ab850768a9c88823b95e02611a36f23444
khalidnecron <bill.ciphermewtwo@gmail.com>
2023-02-24 20:12:25 -0500

    register item

--- src/main/java/io/github/overworld/OverWorldMod.java
+++ src/main/java/io/github/overworld/OverWorldMod.java
@@ -1,16 +1,30 @@
 package io.github.overworld;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.github.overworld.items.InfernalReliquary;
 
 public class OverWorldMod implements ModInitializer {
 
 	public static final String MOD_ID = "overworld";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final InfernalReliquary INFERNAL_RELIQUARY = new InfernalReliquary(null);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.ITEM, new Identifier(MOD_ID, "infernal_reliquary"), INFERNAL_RELIQUARY);
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
+			content.add(INFERNAL_RELIQUARY);
+		});
 	}
 }
